5b77a466065cee4afbdedbf0deb8396eab697db8
Germán Martín <github@gmartin.net>
2024-01-14 23:57:04 +0100

    Update URL

--- firmwareURL.py
+++ firmwareURL.py
@@ -1,2 +1,2 @@
-URL_ESP32S = "https://github.com/G4lile0/tinyGS/releases/latest/download/tinygs_esp32s.bin"
-URL_ESP32S3 = "https://github.com/G4lile0/tinyGS/releases/latest/download/tinygs_esp32s3.bin"
+URL_ESP32S  = "https://static.tinygs.com/firmware/tinygs_esp32s.bin"
+URL_ESP32S3 = "https://static.tinygs.com/firmware/tinygs_esp32s3.bin"
